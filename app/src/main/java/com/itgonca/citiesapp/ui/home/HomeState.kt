cedcaea213bc80c35e0c20ee2bd777df5028b548
Itzae <itzaeg@gmail.com>
2025-07-29 17:44:35 -0600

    HomeState.kt was removed

--- app/src/main/java/com/itgonca/citiesapp/ui/home/HomeState.kt
+++
@@ -1,9 +0,0 @@
-package com.itgonca.citiesapp.ui.home
-
-import com.itgonca.citiesapp.domain.model.City
-
-data class HomeState(
-    val isLoading: Boolean = true,
-    val cities: List<City> = emptyList(),
-    val query: String = ""
-)
